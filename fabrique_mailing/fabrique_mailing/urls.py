b3e5a5d60f56d15d6a45de52da29c8d11dab62c6
Max Bazarov <bazaroffm@gmail.com>
2022-12-16 19:27:46 +0300

    add api url

--- fabrique_mailing/fabrique_mailing/urls.py
+++ fabrique_mailing/fabrique_mailing/urls.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.views.generic import RedirectView
 
 from yasg import urlpatterns as doc_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/v1/', include('mailings.urls')),
 ]
 
 
