68ea3d91eb4f8687d16ece485ed5f293623d3d02
Max Bazarov <bazaroffm@gmail.com>
2022-12-20 01:56:41 +0300

    add celery tasks

--- fabrique_mailing/mailings/api/views.py
+++ fabrique_mailing/mailings/api/views.py
@@ -1,8 +1,23 @@
+from rest_framework.viewsets import ModelViewSet
+
 from mailings.api.serializers import MailingSerializer
 from mailings.models import Mailing
-from rest_framework.viewsets import ModelViewSet
+from mailings.tasks import start_mailing
 
 
 class MailingViewSet(ModelViewSet):
     queryset = Mailing.objects.all()
     serializer_class = MailingSerializer
+
+    def perform_create(self, serializer):
+        serializer.save()
+
+        duration = (
+            serializer.instance.end_at - serializer.instance.start_at
+        ).total_seconds()
+
+        start_mailing.apply_async(
+            args=[serializer.instance.id],
+            eta=serializer.instance.start_at,
+            time_limit=duration
+        )
