0d32f26409f16748f70901de15c04da655a27beb
Max Bazarov <bazaroffm@gmail.com>
2022-12-19 03:11:40 +0300

    add all models

--- fabrique_mailing/users/admin.py
+++ fabrique_mailing/users/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
 
-# Register your models here.
+from .models import Filter, OperatorCode, Tag
+
+Client = get_user_model()
+
+
+@admin.register(Client)
+class ClientAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'phone_number',
+        'operator_code',
+        'tag',
+        'time_zone',
+    )
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name',)
+
+
+@admin.register(OperatorCode)
+class OperatorCodeAdmin(admin.ModelAdmin):
+    list_display = ('id', 'code', )
+
+
+@admin.register(Filter)
+class FilterAdmin(admin.ModelAdmin):
+    list_display = ('id', 'tag', 'operator_code',)
