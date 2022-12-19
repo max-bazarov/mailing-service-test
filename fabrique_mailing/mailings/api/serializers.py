25f7fcd1a403300d095850bed077f16a7df89be8
Max Bazarov <bazaroffm@gmail.com>
2022-12-20 01:56:12 +0300

    add validations for time

--- fabrique_mailing/mailings/api/serializers.py
+++ fabrique_mailing/mailings/api/serializers.py
@@ -1,5 +1,8 @@
-from mailings.models import Mailing
+import datetime
+
 from rest_framework import serializers
+
+from mailings.models import Mailing
 from users.models import Filter, OperatorCode, Tag
 
 
@@ -24,6 +27,20 @@ class MailingSerializer(serializers.ModelSerializer):
     class Meta:
         model = Mailing
         fields = ('message', 'start_at', 'end_at', 'filter')
+
+    def validate_start_at(self, data):
+        if data.timestamp() < datetime.datetime.now().timestamp():
+            raise serializers.ValidationError(
+                'Start time must be in the future'
+            )
+        return data
+
+    def validate_end_at(self, data):
+        if data.timestamp() < datetime.datetime.now().timestamp():
+            raise serializers.ValidationError(
+                'End time must be in the future'
+            )
+        return data
 
     def create(self, validated_data):
         filter_data = validated_data.pop('filter')
