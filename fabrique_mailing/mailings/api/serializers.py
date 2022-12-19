f1f564df6da00dda7608c06ae0b134cf4790997c
Max Bazarov <bazaroffm@gmail.com>
2022-12-19 03:09:51 +0300

    add serilizers

--- fabrique_mailing/mailings/api/serializers.py
+++ fabrique_mailing/mailings/api/serializers.py
@@ -1,33 +1,53 @@
+from mailings.models import Mailing
 from rest_framework import serializers
-
-from mailings.models import Mailing, MailingTag
-from users.models import Tag
+from users.models import Filter, OperatorCode, Tag
 
 
-class TagSerializer(serializers.ModelSerializer):
+class FilterSerializer(serializers.ModelSerializer):
+    tag = serializers.SlugRelatedField(
+        slug_field='name',
+        queryset=Tag.objects.all()
+    )
+    operator_code = serializers.SlugRelatedField(
+        slug_field='code',
+        queryset=OperatorCode.objects.all()
+    )
 
     class Meta:
-        model = Tag
-        fields = ('name',)
+        model = Filter
+        fields = ('tag', 'operator_code')
 
 
 class MailingSerializer(serializers.ModelSerializer):
-    tags = TagSerializer(many=True)
+    filter = FilterSerializer()
 
     class Meta:
         model = Mailing
-        fields = ('message', 'start_at', 'end_at', 'tags')
+        fields = ('message', 'start_at', 'end_at', 'filter')
 
     def create(self, validated_data):
-        tags = validated_data.pop('tags')
-        mailing = Mailing.objects.create(**validated_data)
-
-        for tag in tags:
+        filter_data = validated_data.pop('filter')
+        try:
+            filter = Filter.objects.get(**filter_data)
+        except Filter.DoesNotExist:
             try:
-                tag = Tag.objects.get(**tag)
+                tag = Tag.objects.get(name=filter_data['tag'])
             except Tag.DoesNotExist:
                 raise serializers.ValidationError('Tag does not exist')
 
-            MailingTag.objects.create(mailing=mailing, tag=tag)
+            try:
+                operator_code = OperatorCode.objects.get(
+                    code=filter_data['operator_code']
+                )
+            except OperatorCode.DoesNotExist:
+                raise serializers.ValidationError(
+                    'OperatorCode does not exist'
+                )
+
+            filter = Filter.objects.create(
+                tag=tag,
+                operator_code=operator_code)
+
+        mailing = Mailing.objects.create(filter=filter, **validated_data)
 
         return mailing
