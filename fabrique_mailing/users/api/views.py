5c6c1633b096f98b8d9e6497ca2958893c05ce4a
Max Bazarov <bazaroffm@gmail.com>
2022-12-19 03:12:17 +0300

    add user view set

---
+++ fabrique_mailing/users/api/views.py
@@ -0,0 +1,10 @@
+from django.contrib.auth import get_user_model
+from rest_framework.viewsets import ModelViewSet
+from users.api.serializers import ClientSerializer
+
+Client = get_user_model()
+
+
+class ClientViewSet(ModelViewSet):
+    queryset = Client.objects.all()
+    serializer_class = ClientSerializer
