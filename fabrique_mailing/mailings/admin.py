83b7f8121b8220906e835baca74bd21a9a656590
Max Bazarov <bazaroffm@gmail.com>
2022-12-19 03:09:12 +0300

    add mailing admin

--- fabrique_mailing/mailings/admin.py
+++ fabrique_mailing/mailings/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Mailing
+
+
+@admin.register(Mailing)
+class MailingAdmin(admin.ModelAdmin):
+    list_display = ('id', 'message', 'start_at', 'end_at')
