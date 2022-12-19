300ed2b14abf7b145f4557f84cd68aa1ff0f5b78
Max Bazarov <bazaroffm@gmail.com>
2022-12-19 03:08:51 +0300

    add user viewset

--- fabrique_mailing/fabrique_mailing/urls.py
+++ fabrique_mailing/fabrique_mailing/urls.py
@@ -1,12 +1,13 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import include, path
 from django.views.generic import RedirectView
 
-from yasg import urlpatterns as doc_urls
+from .yasg import urlpatterns as doc_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include('mailings.urls')),
+    path('api/v1/', include('users.urls')),
 ]
 
 
