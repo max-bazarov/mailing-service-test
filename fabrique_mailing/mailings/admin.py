048a0b2d84ecdeafcc147638ad479bf013a1b58e
Max Bazarov <bazaroffm@gmail.com>
2022-12-20 01:55:47 +0300

    register message

--- fabrique_mailing/mailings/admin.py
+++ fabrique_mailing/mailings/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 
-from .models import Mailing
+from .models import Mailing, Message
 
 
 @admin.register(Mailing)
 class MailingAdmin(admin.ModelAdmin):
     list_display = ('id', 'message', 'start_at', 'end_at')
+
+
+@admin.register(Message)
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ('id', 'mailing', 'client', 'sent_at', 'status')
