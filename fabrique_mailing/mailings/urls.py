a334e2db6833fd6f774083a27cfe994016fe56fb
Max Bazarov <bazaroffm@gmail.com>
2022-12-20 20:35:22 +0300

    add stats

--- fabrique_mailing/mailings/urls.py
+++ fabrique_mailing/mailings/urls.py
@@ -4,7 +4,7 @@ from rest_framework.routers import DefaultRouter
 from mailings.api.views import MailingViewSet
 
 router = DefaultRouter()
-router.register(r'mailings', MailingViewSet, basename='mailings')
+router.register('mailings', MailingViewSet, basename='mailings')
 
 urlpatterns = [
     path('', include(router.urls)),
