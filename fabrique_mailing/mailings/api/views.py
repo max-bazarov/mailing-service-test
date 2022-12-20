77abb88bd71f2395b76cf9173bcbd6848c9e0441
Max Bazarov <bazaroffm@gmail.com>
2022-12-20 20:34:47 +0300

    add stats

--- fabrique_mailing/mailings/api/views.py
+++ fabrique_mailing/mailings/api/views.py
@@ -1,13 +1,21 @@
-from rest_framework.viewsets import ModelViewSet
+from rest_framework import viewsets, generics
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
-from mailings.api.serializers import MailingSerializer
-from mailings.models import Mailing
+from mailings.api.serializers import MailingSerializer, MessageSerializer
+from mailings.models import Mailing, Message
 from mailings.tasks import start_mailing
 
 
-class MailingViewSet(ModelViewSet):
+class MailingViewSet(viewsets.ModelViewSet):
     queryset = Mailing.objects.all()
     serializer_class = MailingSerializer
+
+    @action(detail=True)
+    def stats(self, request, pk=None):
+        messages = self.get_object().messages
+        serializer = MessageSerializer(messages, many=True)
+        return Response(serializer.data)
 
     def perform_create(self, serializer):
         serializer.save()
@@ -21,3 +29,10 @@ class MailingViewSet(ModelViewSet):
             eta=serializer.instance.start_at,
             time_limit=duration
         )
+
+
+class MessageList(generics.ListAPIView):
+    serializer_class = MessageSerializer
+
+    def get_queryset(self):
+        return Message.objects.filter(mailing_id=self.kwargs['mailing_id'])
